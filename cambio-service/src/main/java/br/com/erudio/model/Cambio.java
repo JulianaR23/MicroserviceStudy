0873cec9aad6cf3a08cb0da73c6843b07385b931
Juliana <julianarayane23@hotmail.com>
2022-06-30 13:28:03 -0300

    Estudos Microservices

---
+++ cambio-service/src/main/java/br/com/erudio/model/Cambio.java
@@ -0,0 +1,121 @@
+package br.com.erudio.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+
+@Entity(name = "cambio")
+public class Cambio implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "from_currency", nullable = false, length = 3)
+	private String from;
+	
+	@Column(name = "to_currency", nullable = false, length = 3)
+	private String to;
+	
+	@Column(nullable = false)
+	private BigDecimal conversionFactor;
+	
+	@Transient
+	private BigDecimal convertedValue;
+	
+	@Transient
+	private String environment;
+	
+	public Cambio() {};
+	
+	public Cambio(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal convertedValue,
+			String environment) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+		this.convertedValue = convertedValue;
+		this.environment = environment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConversionFactor() {
+		return conversionFactor;
+	}
+
+	public void setConversionFactor(BigDecimal conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+
+	public BigDecimal getConvertedValue() {
+		return convertedValue;
+	}
+
+	public void setConvertedValue(BigDecimal convertedValue) {
+		this.convertedValue = convertedValue;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(conversionFactor, convertedValue, environment, from, id, to);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cambio other = (Cambio) obj;
+		return Objects.equals(conversionFactor, other.conversionFactor)
+				&& Objects.equals(convertedValue, other.convertedValue)
+				&& Objects.equals(environment, other.environment) && Objects.equals(from, other.from)
+				&& Objects.equals(id, other.id) && Objects.equals(to, other.to);
+	}
+	
+	
+	
+}
